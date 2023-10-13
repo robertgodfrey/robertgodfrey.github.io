c3898c7bc205af903b68030efeb6724e5f8c1167
robertgodfrey <robertgodfrey@users.noreply.github.com>
2023-10-13 09:31:05 +0000

    published by GitHub Actions

---
+++ js/about.js
@@ -0,0 +1,106 @@
+// mom's spaghetti. ye have been warned
+const bioText = ' I expect to graduate in May 2024 and plan on pursuing a career as a software engineer.';
+const speed = 50;
+let first = true;
+
+function typeWriter(text, element, cursorStart, cursorEnd, delayTime) {
+  let i = 0;
+
+  function typeWriterAux(text, element) {
+    if (i < text.length) {
+      document.getElementById(element).innerHTML += text.charAt(i);
+      if (text.charAt(i - 1) == '=') {
+        document.getElementById('openingBracket').innerHTML = '[';
+        document.getElementById('closingBracket').style.visibility='visible';
+        document.getElementById('openingBracket').style.color='#ffe835';
+        document.getElementById('closingBracket').style.color='#ffe835';
+        document.getElementById('openingBracket').style.backgroundColor='#3b514d';
+        document.getElementById('closingBracket').style.backgroundColor='#3b514d';
+      } else if (text.charAt(i) == '\'') {
+        if (i == text.length - 1) {
+          document.getElementById('quote').innerHTML = '';
+        } else {
+          document.getElementById('quote').innerHTML = '\'';
+        }
+      }
+      i++;
+      setTimeout(typeWriterAux, 50, text, element);
+    } else if (delayTime <= 3) {
+      flashCursor(cursorStart, delayTime);
+      setTimeout(moveCursor, delayTime * 500, cursorStart, cursorEnd, delayTime);
+    } else if (delayTime == 3.1) {
+      flashCursor(cursorStart, delayTime);
+      setTimeout(typeWriter, delayTime * 500, '\'Application Development\'', 'appDev', 'cursor3', 'cursor3', 3.2);
+      setTimeout(clearBrackets, delayTime * 500);
+    } else if (delayTime == 3.2) {
+      document.getElementById('comma1').innerHTML += ',';
+      setTimeout(addSpace, 80, 'comma1');
+      highlightBrackets();
+      flashCursor(cursorStart, delayTime);
+      setTimeout(typeWriter, delayTime * 500, '\'Cryptography\'', 'crypto', 'cursor3', 'cursor3', 3.3);
+      setTimeout(clearBrackets, delayTime * 500);
+    } else if (delayTime == 3.3) {
+      document.getElementById('comma2').innerHTML += ',';
+      setTimeout(addSpace, 80, 'comma2');
+      highlightBrackets();
+      setTimeout(typeWriter, delayTime * 500, '\'Augmented Reality\'', 'augment', 'cursor3', 'cursor3', 6);
+      setTimeout(clearBrackets, delayTime * 500);
+      flashCursor('cursor3', 20);
+    } else if (delayTime > 5) {
+      highlightBrackets();
+    }
+  }
+  typeWriterAux(text, element);
+}
+
+function addSpace(commaNum) {
+  document.getElementById(commaNum).innerHTML += ' ';
+}
+
+function highlightBrackets() {
+  document.getElementById('openingBracket').style.color='#ffe835';
+  document.getElementById('closingBracket').style.color='#ffe835';
+  document.getElementById('openingBracket').style.backgroundColor='#3b514d';
+  document.getElementById('closingBracket').style.backgroundColor='#3b514d';
+}
+
+function clearBrackets() {
+  document.getElementById('openingBracket').style.color='#a9b7c6';
+  document.getElementById('closingBracket').style.color='#a9b7c6';
+  document.getElementById('openingBracket').style.backgroundColor='#2b2b2b';
+  document.getElementById('closingBracket').style.backgroundColor='#2b2b2b';
+}
+
+function moveCursor(cursorStart, cursorEnd) {
+  document.getElementById(cursorStart).style.visibility='hidden';
+  document.getElementById(cursorEnd).style.visibility='visible';
+  if (cursorStart == 'cursor1') {
+    typeWriter('', 'bottomQuotes', 'cursor2', 'cursor3', 2);
+  } else if (cursorStart == 'cursor2') {
+    typeWriter('interests = ', 'interestsStart', 'cursor3', 'cursor3', 3.1);
+  }
+}
+
+function flashCursor(name, duration) {
+  let j = 0;
+  function flashAux(name, duration) {
+    if (j < duration) {
+      if (j % 2 == 0) {
+        document.getElementById(name).style.visibility='hidden';
+      } else {
+        document.getElementById(name).style.visibility='visible';
+      }
+      j++;
+      setTimeout(flashAux, 500, name, duration);
+    } else if (first) {
+      typeWriter(bioText, 'bio', 'cursor1', 'cursor2', 2);
+      first = false;
+    } else if (duration == 20) {
+      document.getElementById(name).style.visibility='hidden';
+      clearBrackets();
+    }
+  }
+  flashAux(name, duration);
+}
+
+flashCursor('cursor1', 8);
---
+++ js/snek.js
@@ -0,0 +1,297 @@
+const snekButtonGrid = document.getElementById('snek-button-bg');
+const snekGameGrid = document.getElementById('snake-game-grid');
+const buttonSnake = [6, 5, 4, 3, 2, 1, 0];
+const buttonWidth = 20;
+const width = 20;
+const height = 20;
+
+let snake = [2, 1, 0];
+let appleIndex;
+let direction = 1;
+let prevDirection = 1; // to account for fast pressing
+let interval;
+let intervalTime = 200;
+let score = 0;
+let bestScore = 0;
+
+let highScoreOne, highScoreTwo, highScoreThree;
+let highScores = [];
+
+async function getHighScores() {
+  const response = await fetch('https://nw3wvp7zk7zlvqctaqes5xs7ji0enbih.lambda-url.us-east-1.on.aws');
+  const scores = await response.json();
+  Object.keys(scores).forEach((score) => highScores.push(score));
+  highScores = highScores.reverse();
+  highScoreOne = `${highScores[0]} - ${scores[highScores[0]]}`;
+  highScoreTwo = `${highScores[1]} - ${scores[highScores[1]]}`;
+  highScoreThree = `${highScores[2]} - ${scores[highScores[2]]}`;
+}
+
+async function postScore(e) {
+  document.getElementById('high-score').style.display = 'none';
+  document.getElementById('loader').classList.add('loader');
+  document.getElementById('loader').classList.remove('loader-hidden');
+  document.getElementById('high-score-1').innerHTML = '';
+  document.getElementById('high-score-2').innerHTML = '';
+  document.getElementById('high-score-3').innerHTML = '';
+  const response = await fetch('https://nq3q5hwyijmmfhbjcgbt6v57hm0wbkqj.lambda-url.us-east-1.on.aws/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: `${document.getElementById('highScoreName').value}~*&*~${score}`,
+  });
+  const scores = await response.json();
+  Object.keys(scores).forEach((score) => highScores.push(score));
+  highScores = highScores.reverse();
+  highScoreOne = `${highScores[0]} - ${scores[highScores[0]]}`;
+  highScoreTwo = `${highScores[1]} - ${scores[highScores[1]]}`;
+  highScoreThree = `${highScores[2]} - ${scores[highScores[2]]}`;
+  document.getElementById('high-score-1').innerHTML = highScoreOne;
+  document.getElementById('high-score-2').innerHTML = highScoreTwo;
+  document.getElementById('high-score-3').innerHTML = highScoreThree;
+
+  document.getElementById('loader').classList.add('loader-hidden');
+  document.getElementById('loader').classList.remove('loader');
+}
+
+function moveSnakeButton(squares) {
+  let tail = buttonSnake.pop();
+  squares[tail].classList.remove('snake');
+  buttonSnake.unshift(buttonSnake[0] + direction);
+  squares[buttonSnake[0]].classList.add('snake');
+  switch (buttonSnake[0]) {
+    case 19:
+      // top right of button
+      direction = buttonWidth;
+      break;
+    case 99:
+      // bottom right
+      direction = -1;
+      break;
+    case 80:
+      // bottom left
+      direction = -buttonWidth;
+      break;
+    case 0:
+      // top left
+      direction = 1;
+      break;
+    case 4:
+      direction = buttonWidth;
+      break;
+    case 64:
+      direction = 1;
+      break;
+    case 74:
+      direction = -buttonWidth;
+      break;
+    case 14:
+      direction = 1;
+      break;
+    case 93:
+      direction = -buttonWidth;
+      break;
+    case 33:
+      direction = -1;
+      break;
+    case 25:
+      direction = buttonWidth;
+      break;
+    case 85:
+      direction = -1;
+      break;
+  }
+}
+
+const hit = (squares) => {
+  if (snake[0] + direction === snake[1]) {
+    direction = prevDirection;
+  }
+  if (
+    (snake[0] + width >= width * width && direction === width) ||
+    (snake[0] % width === width - 1 && direction === 1) ||
+    (snake[0] % width === 0 && direction === -1) ||
+    (snake[0] - width <= 0 && direction === -width) ||
+    squares[snake[0] + direction].classList.contains('snake')
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function genApple(squares) {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains('snake'));
+  squares[appleIndex].classList.add('apple');
+}
+
+function eatApple(squares, tail) {
+  if (squares[snake[0]].classList.contains('apple')) {
+    squares[snake[0]].classList.remove('apple');
+    squares[tail].classList.add('snake');
+    snake.push(tail);
+    genApple(squares);
+    score++;
+    if (score > bestScore) {
+      bestScore = score;
+      document.getElementById('best-score').innerHTML = score;
+    }
+    document.getElementById('score').innerHTML = score;
+    clearInterval(interval);
+    if (intervalTime > 72) {
+      intervalTime = intervalTime * 0.95;
+      document.getElementById('current-speed').innerHTML = Math.round(200 / intervalTime * 35);
+    } else if (Math.round(intervalTime) === 72) {
+      intervalTime = 70;
+      document.getElementById('current-speed').innerHTML = 100;
+    }
+    interval = setInterval(moveSnake, intervalTime, squares);
+  }
+}
+
+function moveSnake(squares) {
+  if (hit(squares)) {
+    // game over
+    clearInterval(interval);
+    if (score > highScores[2]) {
+      document.getElementById('high-score').style.display = 'inline';
+    }
+    document.getElementById('game-over').style.display = 'inline';
+    document.getElementById('high-score-1').innerHTML = highScoreOne;
+    document.getElementById('high-score-2').innerHTML = highScoreTwo;
+    document.getElementById('high-score-3').innerHTML = highScoreThree;
+    document.getElementById('last-score').innerHTML = score;
+    document.addEventListener('keydown', enterToRestart);
+    document.removeEventListener('keydown', control);
+  } else {
+    let tail = snake.pop();
+    squares[tail].classList.remove('snake');
+    snake.unshift(snake[0] + direction);
+    eatApple(squares, tail);
+    squares[snake[0]].classList.add('snake');
+  }
+}
+
+function control(e) {
+  if (e.code === 'ArrowRight' || e.code === 'KeyD') {
+    e.preventDefault();
+    if (direction === -1) {
+      return;
+    }
+    prevDirection = direction;
+    direction = 1; // right one div
+  } else if (e.code === 'ArrowUp' || e.code === 'KeyW') {
+    e.preventDefault();
+    if (direction === width) {
+      return;
+    }
+    prevDirection = direction;
+    direction = -width; // snake goes 20 divs up
+  } else if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
+    e.preventDefault();
+    if (direction === 1) {
+      return;
+    }
+    prevDirection = direction;
+    direction = -1; // left one div
+  } else if (e.code === 'ArrowDown' || e.code === 'KeyS') {
+    e.preventDefault();
+    if (direction === -width) {
+      return;
+    }
+    prevDirection = direction;
+    direction = +width; // down 20 divs
+  }
+}
+
+function enterToRestart(e) {
+  if (e.code === 'Enter') {
+    restartGame();
+  }
+}
+
+function startGame() {
+  const squares = document.querySelectorAll('.snake-game-grid div');
+  snake.forEach((index) => squares[index].classList.add('snake'));
+  document.addEventListener('keydown', control);
+  genApple(squares);
+  intervalTime = 200;
+  direction = 1;
+  interval = setInterval(moveSnake, 200, squares);
+}
+
+function restartGame() {
+  document.removeEventListener('keydown', enterToRestart);
+  document.getElementById('game-over').style.display = 'none';
+  const squares = document.querySelectorAll('.snake-game-grid div');
+  snake.forEach((index) => squares[index].classList.remove('snake'));
+  squares[appleIndex].classList.remove('apple');
+  snake = [2, 1, 0];
+  score = 0;
+  document.getElementById('score').innerHTML = 0;
+  document.getElementById('current-speed').innerHTML = 35;
+  startGame();
+}
+
+snekButtonGrid.onclick = () => {
+  // stop button animation, show game board
+  document.getElementById('pic-snake-button').style.display = 'none';
+  document.getElementById('snake-game').style.display = 'inline';
+  clearInterval(interval);
+
+  for (let i = 0; i < 400; i++) {
+    snekGameGrid.append(document.createElement('div'));
+  }
+};
+
+document.getElementById('snake-start-btn').onclick = () => {
+  document.getElementById('snake-game-instructions').style.display = 'none';
+  startGame();
+  getHighScores();
+}
+
+document.getElementById('snake-restart-btn').onclick = () => {
+  restartGame();
+}
+
+document.getElementById('left-arrow').onclick = () => {
+  if (direction === 1) {
+    return;
+  }
+  prevDirection = direction;
+  direction = -1;
+}
+
+document.getElementById('up-arrow').onclick = () => {
+  if (direction === width) {
+    return;
+  }
+  prevDirection = direction;
+  direction = -width; // snake goes 20 divs up
+}
+
+document.getElementById('right-arrow').onclick = () => {
+  if (direction === -1) {
+    return;
+  }
+  prevDirection = direction;
+  direction = 1; // right one div
+}
+
+document.getElementById('down-arrow').onclick = () => {
+  if (direction === -width) {
+    return;
+  }
+  prevDirection = direction;
+  direction = +width; // down 20 divs
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (let i = 0; i < 100; i++) {
+    snekButtonGrid.append(document.createElement('div'));
+  }
+  const squares = document.querySelectorAll('.snek-button-board div');
+  buttonSnake.forEach((index) => squares[index].classList.add('snake'));
+
+  interval = setInterval(moveSnakeButton, 200, squares);
+});
